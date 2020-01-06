654fd69d86657b121d7de0f4c81190e83f38512f
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-01-05 16:29:17 -0800

    Add data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,21 @@
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Einstein",
+        "photo": "https://d14e8oeg5e788p.cloudfront.net/content/53451/747e9a3bbe0a3b6d427690f3abd2dcfe.jpg",
         "scores": [5,1,4,4,5,1,2,5,4,1]
-    }
-]
+    },
+    {
+        "name": "Princess Diane",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQzMzAxMjk3OTQ3Mjg5MTYw/diana_gettyimages-515185764jpg.jpg",
+        "scores": [5,1,4,5,5,3,2,2,3,4]
+    },
+    {
+        "name": "Walter Disney",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/51CwCRB7OlL._SX466_.jpg",
+        "scores": [1,4,3,5,1,1,3,4,5,3]
+    },
+
+];
+
+module.exports = friends;
+
