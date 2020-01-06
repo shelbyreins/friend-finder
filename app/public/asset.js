b1ae8d90cfa04a19acb438232a19e6abd2c9d004
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-01-05 16:30:13 -0800

    Comment out for now

--- app/public/asset.js
+++ app/public/asset.js
@@ -1,27 +1,40 @@
-$("#submit").on("click", function(event){
-    event.preventDefault();
+// $("#submit").on("click", function (event) {
+//     event.preventDefault();
 
-    var user = {
-        name: $("#name").val().trim(),
-        photo: $("#photo").val().trim(),
-        scores: [
-            $("#question1").val(),
-            $("#question2").val(),
-            $("#question3").val(),
-            $("#question4").val(),
-            $("#question5").val(),
-            $("#question6").val(),
-            $("#question7").val(),
-            $("#question8").val(),
-            $("#question9").val(),
-            $("#question10").val()
-        ]
-    }
+//     var valid = true;
 
-    $.post("/api/friends", user,
-    function(data){
-        $("#textbox").text(data.name)
-        $("#textbox2").attr("src", data.photo)
-    });
-    
-})
+//     if ($("#name").val() === "" || $("#photo").val() === "") {
+//         valid = false;
+//     } else if ($("#question1").val() === "" || $("#question2").val() === "" || $("#question3").val() === "" || $("#question4").val() === "" || $("#question5").val() === "" || $("#question6").val() === "" || $("#question7").val() === "" || $("#question8").val() === "" || $("#question9").val() === "" || $("#question10").val() === "") {
+//         valid = false;
+//     }
+
+//     if (valid === true) {
+
+//         var user = {
+//             name: $("#name").val().trim(),
+//             photo: $("#photo").val().trim(),
+//             scores: [
+//                 $("#question1").val(),
+//                 $("#question2").val(),
+//                 $("#question3").val(),
+//                 $("#question4").val(),
+//                 $("#question5").val(),
+//                 $("#question6").val(),
+//                 $("#question7").val(),
+//                 $("#question8").val(),
+//                 $("#question9").val(),
+//                 $("#question10").val()
+//             ]
+//         };
+
+//         $.post("/api/friends", user,
+//             function (data) {
+//                 $("#match-name").text(data.name)
+//                 $("#match-image").attr("src", data.photo)
+//             });
+
+//     } else {
+//         alert("Please complete all questions")
+//     }
+// });
